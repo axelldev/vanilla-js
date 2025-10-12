8b24421ba16af1f9c2d5d7432585d5fcb6003a9f
axelldev <axellsolisrubio@gmail.com>
2025-10-11 22:52:04 -0600

    feat: load css for custom elements

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,13 +1,22 @@
 export class MenuPage extends HTMLElement {
   constructor() {
     super();
+    this.root = this.attachShadow({ mode: "open" });
+    this.loadCSS();
+  }
+
+  async loadCSS() {
+    const req = await fetch("/components/MenuPage.css");
+    const css = await req.text();
+    const styles = document.createElement("style");
+    this.root.styles = this.root.appendChild(styles);
+    styles.textContent = css;
   }
 
   connectedCallback() {
-    const root = this.attachShadow({ mode: "open" });
     const template = document.getElementById("menu-page-template");
     const content = template.content.cloneNode(true);
-    root.appendChild(content);
+    this.root.appendChild(content);
   }
 }
 
